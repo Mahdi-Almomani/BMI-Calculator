52bb4c7c73d6bc93e9def0c827393de01fcbe4f3
Mahdi-Almomani <mahdealmomani1534119@gmail.com>
2022-12-24 12:38:25 +0300

    u

---
+++ lib/mahdi.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class Mahdi extends StatefulWidget {
+  const Mahdi({super.key});
+
+  @override
+  State<Mahdi> createState() => _MahdiState();
+}
+
+class _MahdiState extends State<Mahdi> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
